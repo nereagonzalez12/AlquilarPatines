17c43080a17532b4d19cc4d0e94fa89515ac77aa
Nerea González Selva <47923571+nereagonzalez12@users.noreply.github.com>
2024-02-08 14:36:53 +0100

    api instalada

---
+++ patines/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PatinesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'patines'
---
+++ patines/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from patines import views
+
+urlpatterns = [
+    # path('', include(router.urls)),
+]
